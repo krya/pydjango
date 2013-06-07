a42c9d3312d4cec4984c93af556a17349cd27d9f
Denis K <sinedone@gmail.com>
2013-06-07 10:51:17 +0300

    pep-8

--- pydjango/db_reuse.py
+++ pydjango/db_reuse.py
@@ -11,6 +11,7 @@ from django.conf import settings
 from django.db import connections, transaction
 from django.db.backends.sqlite3.base import DatabaseOperations as BDO
 
+
 def can_support_db_reuse(connection):
     """Return whether it makes any sense to use REUSE_DB with the backend of a connection."""
     # This is a SQLite in-memory DB. Those are created implicitly when
@@ -35,6 +36,7 @@ def test_database_exists_from_previous_run(connection):
         connection.close()
         connection.settings_dict['NAME'] = orig_db_name
 
+
 def create_test_db(self, verbosity=1, autoclobber=False):
     """
     This method is a monkey patched version of create_test_db that
@@ -57,9 +59,11 @@ def create_test_db(self, verbosity=1, autoclobber=False):
 
     return test_database_name
 
+
 def _get_test_db_name(self):
     name = super(self.__class__, self)._get_test_db_name()
     return name + self.db_postfix
+
 
 def monkey_patch_creation_for_db_reuse(db_postfix, force=False):
     for alias in connections:
@@ -79,10 +83,13 @@ class BaseDatabaseOperations(BDO):
 
     def savepoint_create_sql(self, sid):
         return "SAVEPOINT %s" % sid
+
     def savepoint_commit_sql(self, sid):
         return "RELEASE SAVEPOINT %s" % sid
+
     def savepoint_rollback_sql(self, sid):
         return "ROLLBACK TO SAVEPOINT %s" % sid
+
 
 def cursor_wrapper(function):
     def wraps(*args, **kwargs):
@@ -98,15 +105,16 @@ def cursor_wrapper(function):
         return cursor
     return wraps
 
+
 def wrap_database():
     for db in connections:
         if connections[db].vendor == 'sqlite':
             options = settings.DATABASES[db].get("OPTIONS", {})
             # isolation_level should be None to use savepoints in sqlite
-            options.update({'isolation_level':None})
+            options.update({'isolation_level': None})
             settings.DATABASES[db]['OPTIONS'] = options
             connections[db].features.uses_savepoints = True
-            if django.VERSION < (1,4) :
+            if django.VERSION < (1, 4):
                 connections[db].ops = BaseDatabaseOperations()
             else:
                 connections[db].ops = BaseDatabaseOperations(db)
