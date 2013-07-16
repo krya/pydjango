ef38b168dd269173b4bf707c307288f10904b6cc
Denis K <sinedone@gmail.com>
2013-07-16 17:50:25 +0300

    use swappable model if available

--- pydjango/fixtures.py
+++ pydjango/fixtures.py
@@ -3,7 +3,14 @@
 import pytest
 
 from django.conf import settings
-from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth.models import AnonymousUser
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+except ImportError:
+    from django.contrib.auth.models import User
+
 from django.test.client import Client
 from django.contrib.auth import login
 from django.utils.importlib import import_module
